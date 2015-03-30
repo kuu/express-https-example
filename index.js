bc6514b9c4e080f35941d02d1802a17ccac38511
Kuu Miyazaki <miyazaqui@gmail.com>
2015-03-30 14:31:17 +0000

    Remove certs and private key from the repo.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 'use strict';
 
 var https = require('https'),
+    config = require('config'),
     fs = require('fs'),
     path = require('path'),
     express = require('express');
 
 var app = express(),
-    port = process.env.PORT || 443,
+    port = process.env.PORT || config.port,
     BASE_DIR = path.join(__dirname, '.'),
-    DOCS_DIR = path.join(BASE_DIR, 'www'),
-    TLS_DIR = path.join(__dirname, 'tls'),
+    DOCS_DIR = path.join(BASE_DIR, config.dir.docroot),
+    TLS_DIR = path.join(BASE_DIR, config.dir.tls),
     DOCS_PATH = path.join(DOCS_DIR, 'index.html');
 
 app.set('port', port);
@@ -21,14 +22,13 @@ app.get('/', function (req, res) {
 
 // Start server
 if (require.main === module) {
-  var options = {
-    key: fs.readFileSync(path.join(TLS_DIR, 'flightplan_gree-dev_net.key')),
-    cert: fs.readFileSync(path.join(TLS_DIR, 'flightplan_gree-dev_net.crt')),
-    ca: [
-      fs.readFileSync(path.join(TLS_DIR, 'CrossTrustOVCA3.crt')),
-      fs.readFileSync(path.join(TLS_DIR, 'SecurityCommunicationRootCA2.crt'))
-    ]
-  };
+  var caList = config.tls.file.caList.map(function (filepath) {
+        return fs.readFileSync(path.join(TLS_DIR, filepath));
+      }), options = {
+        key: fs.readFileSync(path.join(TLS_DIR, config.tls.file.key)),
+        cert: fs.readFileSync(path.join(TLS_DIR, config.tls.file.cert)),
+        ca: caList
+      };
 
   https.createServer(options, app).listen(app.get('port'), function () {
     console.log('Express started in ' + app.get('env') + ' mode on port ' + app.get('port') + '.');
